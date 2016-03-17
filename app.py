56c7316bbe254e8fbf662c48e8a9cc092e8cf64c
muhdamrullah <muneorion@gmail.com>
2016-03-17 11:37:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,23 @@ import subprocess
 
 app = Flask(__name__)
 
-# Create a new instance of a ChatBot
-bot = ChatBot("Terminal",
-    storage_adapter="chatterbot.adapters.storage.JsonDatabaseAdapter",
-    logic_adapters=[
-        "chatterbot.adapters.logic.EvaluateMathematically",
-        "chatterbot.adapters.logic.ClosestMatchAdapter"
-    ],
-    io_adapters=[
-        "chatterbot.adapters.io.TerminalAdapter"
-    ],
-    database="../database.db")
-
-user_input = "Type something to begin..."
-
-@app.route('/chatbot', methods=['POST'])
-def getReply():
+@app.route('/chatbot/<database_name>', methods=['POST'])
+def getReply(database_name):
     try:
+    	# Create a new instance of a ChatBot
+    	database_directory = "%s.db" % database_name
+	bot = ChatBot("Terminal",
+    	    storage_adapter="chatterbot.adapters.storage.JsonDatabaseAdapter",
+    	    logic_adapters=[
+        	"chatterbot.adapters.logic.EvaluateMathematically",
+        	"chatterbot.adapters.logic.ClosestMatchAdapter"
+    	    ],
+    	    io_adapters=[
+        	"chatterbot.adapters.io.TerminalAdapter"
+    	    ],
+    	    database=database_directory)
+
+	user_input = "Type something to begin..."
 	chat_message = request.json['message']
         bot_input = bot.get_response(chat_message)
 	return bot_input
@@ -30,4 +30,4 @@ def getReply():
 
 if __name__=='__main__':
     app.debug = True
-    app.run('0.0.0.0', 12000)
+    app.run('0.0.0.0', 8080)
