88c193d90b938d96e0021c4e6cbfb090bfb5a544
muhdamrullah <muneorion@gmail.com>
2016-03-17 15:25:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,42 @@ from flask import request
 import subprocess
 
 app = Flask(__name__)
+list_of_database = ['example']
+
+# Check for duplication
+def checkDatabase(database_example):
+    global list_of_database
+    if database_example in list_of_database[-1]:
+	return False
+    else:
+        list_of_database.append(database_example)
+	return True
+
+# Create a new instance of a ChatBot
+def createDB(database_example):
+    database_directory = "%s.db" % database_example
+    createDB.bot = ChatBot("Terminal",
+        storage_adapter="chatterbot.adapters.storage.JsonDatabaseAdapter",
+        logic_adapters=[
+            "chatterbot.adapters.logic.EvaluateMathematically",
+            "chatterbot.adapters.logic.ClosestMatchAdapter"
+        ],
+        io_adapters=[
+            "chatterbot.adapters.io.TerminalAdapter"
+        ],
+        database=database_directory)
 
 @app.route('/chatbot/<database_name>', methods=['POST'])
 def getReply(database_name):
     try:
-    	# Create a new instance of a ChatBot
-    	database_directory = "%s.db" % database_name
-	bot = ChatBot("Terminal",
-    	    storage_adapter="chatterbot.adapters.storage.JsonDatabaseAdapter",
-    	    logic_adapters=[
-        	"chatterbot.adapters.logic.EvaluateMathematically",
-        	"chatterbot.adapters.logic.ClosestMatchAdapter"
-    	    ],
-    	    io_adapters=[
-        	"chatterbot.adapters.io.TerminalAdapter"
-    	    ],
-    	    database=database_directory)
-
-	user_input = "Type something to begin..."
-	chat_message = request.json['message']
-        bot_input = bot.get_response(chat_message)
-	return bot_input
+	if checkDatabase(database_name):
+            print database_name
+	    createDB(database_name)
+        else:
+            print "Accessing existing database..."
+        chat_message = request.json['message']
+        bot_input = createDB.bot.get_response(chat_message)
+        return bot_input
     except (KeyboardInterrupt, EOFError, SystemExit):
       pass
 
